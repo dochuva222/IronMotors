7e90c3b68af91395fa6ae4a892c9dbebaad39a01
DocHuva <shpalitov222@gmail.com>
2022-04-30 20:01:49 +0300

    asdf

--- IronMotors/IronMotors/Pages/MainWindowPages/ProfilePages/MyDataPage.xaml.cs
+++ IronMotors/IronMotors/Pages/MainWindowPages/ProfilePages/MyDataPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IronMotors.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,7 +29,45 @@ namespace IronMotors.Pages.MainWindowPages.ProfilePages
 
         private void BSave_Click(object sender, RoutedEventArgs e)
         {
+            string errorMessage = "";
+            if (!MyValidator.Validate(App.LoggedClient, out errorMessage))
+            {
+                MessageBox.Show(errorMessage, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (App.DB.Client.Where(c => c.Id != App.LoggedClient.Id).FirstOrDefault(c => c.PhoneNumber == App.LoggedClient.PhoneNumber) != null)
+            {
+                MessageBox.Show("Данный номер уже используется", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (App.DB.Client.Where(c => c.Id != App.LoggedClient.Id).FirstOrDefault(c => c.Email == App.LoggedClient.Email) != null)
+            {
+                MessageBox.Show("Данная электронная почта уже используется", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            App.DB.SaveChanges();
+            MessageBox.Show("Данные успешно изменены", "Успешно", MessageBoxButton.OK);
+        }
 
+        private void BChangePassword_Click(object sender, RoutedEventArgs e)
+        {
+            if(PBOldPassword.Password != App.LoggedClient.Password)
+            {
+                MessageBox.Show("Старый пароль не совпадает", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if(PBNewPassword.Password != PBRepeatPassword.Password)
+            {
+                MessageBox.Show("Пароли не совпадают", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            App.LoggedClient.Password = PBNewPassword.Password;
+            App.DB.SaveChanges();
+            PBOldPassword.Clear();
+            PBNewPassword.Clear();
+            PBRepeatPassword.Clear();
+            MessageBox.Show("Пароль успешно изменен", "Успешно", MessageBoxButton.OK);
         }
     }
 }
